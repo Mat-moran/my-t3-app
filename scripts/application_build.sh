da440adc5ad362f8df5b2264665e22ec19a21c07
Amadeo Morán <amadeo.moran@gmail.com>
2022-08-03 09:52:04 +0200

    [ADD] new step

---
+++ scripts/application_build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/ec2-user/t3-frontend
+nvm use 16
+npm run build
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 cd /home/ec2-user/t3-frontend
 nvm use 16
-npm run build
 pm2 start npm --name "myt3app" -- start
 pm2 startup
 pm2 save
